5dea432d79e46cd11f8d62d721ceb8d92d9a021a
Isreal6ix <isrealsix@gmail.com>
2021-08-13 02:33:46 +0700

    created components for Section and Home

---
+++ src/components/Section.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Section = () => {
+	return <Wrap>Section</Wrap>;
+};
+
+export default Section;
+
+const Wrap = styled.div``;
