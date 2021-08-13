d564b80fc77c0e03693e16b6bd7a38a185e377ae
Isreal6ix <isrealsix@gmail.com>
2021-08-13 20:23:32 +0700

    added the downArrow svg and added a keyframe animation

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,6 +8,14 @@ const Section = () => {
 				<h1>Model S</h1>
 				<p>Order Online for Touch Delivery</p>
 			</ItemText>
+			<Buttons>
+				<ButtonGroup>
+					<LeftButton>Custom Order</LeftButton>
+
+					<RightButton>Existing Inventory</RightButton>
+				</ButtonGroup>
+				<DownArrow src="/images/down-arrow.svg" />
+			</Buttons>
 		</Wrap>
 	);
 };
@@ -21,9 +29,45 @@ const Wrap = styled.div`
 	background-position: center;
 	bacground-repeat: no-repeat;
 	background-image: url('/images/model-s.jpg');
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
 `;
 
 const ItemText = styled.div`
 	padding-top: 15vh;
 	text-align: center;
 `;
+
+const ButtonGroup = styled.div`
+	display: flex;
+	margin-bottom: 30px;
+`;
+
+const LeftButton = styled.div`
+	background-color: rgba(23, 26, 32, 0.8);
+	height: 40px;
+	width: 256px;
+	color: #fff;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 100px;
+	opacity: 0.85;
+	text-transform: upprcase;
+	font-size: 12px;
+	cursor: pointer;
+	margin: 8px;
+`;
+
+// Inherit the styles of LeftButton
+const RightButton = styled(LeftButton)``;
+
+const DownArrow = styled.img`
+	margin-top: 20px;
+	height: 40px;
+	animation: animateDown infinite 1.5s;
+`;
+
+const Buttons = styled.div``;
